966583eec9112ba093a0e331ad35da2f16bd5b23
Kaidong Hu <dongforregister@gmail.com>
2017-11-19 15:09:30 -0500

    Maxout integrated

--- lc/graph.py
+++ lc/graph.py
@@ -4,3 +4,21 @@
    All graphs used in tensorflow will be defined in this module,
    will be imported as differend classes.
 """
+
+from . import config
+
+def max_out(inputs, num_units=config.NUM_UNIT, axis=None):
+    shape = inputs.get_shape().as_list()
+    if shape[0] is None:
+        shape[0] = -1
+    if axis is None:  # Assume that channel is the last dimension
+        axis = -1
+    num_channels = shape[axis]
+    if num_channels % num_units:
+        raise ValueError('number of features({}) is not '
+                         'a multiple of num_units({})'.format(
+                             num_channels, num_units))
+    shape[axis] = num_units
+    shape += [num_channels // num_units]
+    outputs = tf.reduce_max(tf.reshape(inputs, shape), -1, keep_dims=False)
+    return outputs
